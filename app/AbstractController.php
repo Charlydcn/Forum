aeb82c63cd30d5e98c17a905d75fd6f98b2ce98a
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-19 11:02:19 +0200

    (abstCtrl) modified redirectTo method

--- app/AbstractController.php
+++ app/AbstractController.php
@@ -8,9 +8,9 @@
         public function redirectTo($ctrl = null, $action = null, $id = null){
 
             if($ctrl != "home"){
-                $url = $ctrl ? "/".$ctrl : "";
-                $url.= $action ? "/".$action : "";
-                $url.= $id ? "/".$id : "";
+                $url = $ctrl ? "?ctrl=".$ctrl : "";
+                $url.= $action ? "&action=".$action : "";
+                $url.= $id ? "&id=".$id : "";
                 $url.= ".html";
             }
             else $url = "/";
