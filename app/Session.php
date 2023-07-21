fd2f2df1fe82df7e2072679e513a56e3a59cdd67
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-21 15:45:13 +0200

    (session) changeed admin role name to 'admin'

--- app/Session.php
+++ app/Session.php
@@ -38,7 +38,7 @@
         }
 
         public static function isAdmin(){
-            if(self::getUser() && self::getUser()->hasRole("ROLE_ADMIN")){
+            if(self::getUser() && self::getUser()->hasRole("admin")){
                 return true;
             }
             return false;
