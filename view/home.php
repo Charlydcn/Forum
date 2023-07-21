81242903555f4bf296c5e2244eb761e33a6a2a1b
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-21 11:05:09 +0200

    (v/home) removed login form

--- view/home.php
+++ view/home.php
@@ -1,13 +1,1 @@
 <h1>Welcome to the Forum</h1>
-
-<form action="index.php?ctrl=security&action=login" method="POST" autocomplete="off">
-    <h1>Log in</h1>
-    <label>Username
-        <input type="text" name="username" required>
-    </label>
-    <label>Password
-        <input type="password" name="password" required>
-    </label>
-    <input type="submit" name="submit" value="Log in">
-</form>
-<p>Not registered yet ? <a href="index.php?ctrl=security&action=displayRegister">Register</a></p>
