7fd81ca474b7f4137fdd0695ceb44d2e2c1f6b8b
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-21 15:51:43 +0200

    (v/userDts) view with form or <p> depends on role

---
+++ view/user/userDetails.php
@@ -0,0 +1,49 @@
+<?php
+
+$user = $result['data']['user'];
+
+if($_SESSION['user']->getRole() === 'admin') {
+?>
+
+<form action="index.php?ctrl=security&action=editUser&id=<?=$user->getId()?>" method="POST" autocomplete="off">
+    <label>
+        Username :
+        <input type="text" name="username" value="<?= $user->getUsername() ?>">
+    </label>
+
+    <label>
+        E-Mail :
+        <input type="email" name="email" value="<?= $user->getEmail() ?>">
+    </label>
+
+    <label>
+        Password :
+        <input type="password" name="pass1" value="">
+    </label>
+    <label>
+        Confirm password :
+        <input type="password" name="pass2" value="">
+    </label>
+
+    <select name="role">
+        <option value="<?= $user->getRole() ?>"><?= $user->getRole() ?></option>
+        <option value="user">user</option>
+    </select>
+    
+    <p>Member since : <?=$user->getRegistrationDate()?></p>   
+    
+    <input type="submit" name="submit" value="Save changes">
+
+    <a href="index.php?ctrl=security&action=deleteUser&id=<?=$user->getId()?>">
+        Delete user
+    </a>
+</form>
+  
+<?php } else { ?>
+
+    <p>Username : <?=$user->getUsername()?></p>
+    <p>E-Mail : <?=$user->getEmail()?></p>
+    <p>Member since : <?=$user->getRegistrationDate()?></p>
+
+
+<?php } ?>
