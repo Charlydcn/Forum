960befe569aace5eb450aed24dc261fc429c9d30
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-21 15:47:32 +0200

    (userMngr) +editUser/UserPswrd, deleteUser methods

--- model/managers/UserManager.php
+++ model/managers/UserManager.php
@@ -14,6 +14,36 @@
             parent::connect();
         }
 
+        public function editUser($id, $username, $email, $role)
+        {
+            $sql = "UPDATE user
+                SET username = :username, email = :email, role = :role
+                WHERE id_user = :id";
 
+            DAO::update(
+                $sql,
+                ["id" => $id,
+                "username" => $username,
+                "email" => $email,
+                "role" => $role]);
+
+        }
+
+        public function editUserPassword($id, $password)
+        {
+            $sql = "UPDATE user
+            SET password = :password
+            WHERE id_user = :id";
+
+            DAO::update($sql,
+            ["id" => $id,
+            "password" => $password]);
+
+        }
+
+        public function deleteUser($id)
+        {
+            delete($id, $tableName);
+        }
 
     }
