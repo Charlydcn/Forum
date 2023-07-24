98a49d35cf17f5e372732e8a849a31948bb3e7ae
Charlydcn <charly.ducournau@outlook.fr>
2023-07-24 16:55:27 +0200

    (postManager) added comment

--- model/managers/PostManager.php
+++ model/managers/PostManager.php
@@ -15,4 +15,31 @@ class PostManager extends Manager
     {
         parent::connect();
     }
+
+    // public function listPostsByTopic($id)
+    // {
+    //     $sql = "SELECT *
+    //             FROM post
+    //             WHERE topic_id = :id";
+
+    //     return $this->getMultipleResults(
+    //         DAO::select($sql, ["id" => $id]),
+    //         $this->className
+    //     );
+    // }
+
+    // public function listPostsByTopic($id)
+    // {
+
+    //     $postManager = new PostManager();
+    //     $posts = $postManager->findMultipleBy('topic_id', $id);
+
+    //     return [
+    //         "view" => VIEW_DIR . "forum/postsByTopic.php",
+    //         "data" => [
+    //             "posts" => $posts
+    //         ]
+    //     ];
+    // }
+
 }
