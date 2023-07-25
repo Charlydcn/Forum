13352574c07ff122dbd5802d61b740efe4b1f8ca
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-25 15:54:22 +0200

    (v/posts) displaying posts and form to create one

--- view/forum/postsByTopic.php
+++ view/forum/postsByTopic.php
@@ -1,11 +1,46 @@
 <?php 
 $posts = $result['data']['posts'];
+
 ?>
 
 <?php
 foreach ($posts as $post) {
 ?>
 
-<a href=""><?=$post->getContent()?></a>
+    <div> <!-- POST HEADER -->
+        <a href="index.php?ctrl=forum&action=postDetails&id=<?=$post->getId()?>"><?= $post->getTitle() ?></a>
+        <a href="index.php?ctrl=forum&action=userDetails&id=<?=$post->getUser()->getId()?>">
+            <?= $post->getUser()->getUsername()?>
+        </a>
+
+        <?= $post->getCreationDate() ?>
+        <?php
+        if($post->getUser()->getId() === $_SESSION['user']->getId() || App\Session::isAdmin()) {        
+        ?>
+
+        <a href="index.php?ctrl=security&action=deletePost&id=<?=$post->getId()?>" class="delete-btn">
+            <i class="fa-solid fa-trash-can"></i>
+        </a>
+        
+        <?php } ?>
+    </div>
+
+    <div> <!-- POST MAIN -->
+        <?= html_entity_decode($post->getContent()) ?>
+    </div>
+    <br>
+
+<?php
+}
+if(isset($_SESSION['user'])) {
+?>
+
+<form action="index.php?ctrl=security&action=createPost&id=<?=$post->getTopic()->getId()?>" method="POST">
+
+    <textarea class="post" name="newPost"></textarea>
+
+    <input type="submit" name="submit" value="Send post">
+
+</form>
 
 <?php } ?>
