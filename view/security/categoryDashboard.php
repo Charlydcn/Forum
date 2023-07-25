21f4fb9c23172ebf628a272d0eb31cd9bbc57a04
Charlydcn <Charly.ducournau@outlook.fr>
2023-07-25 09:28:39 +0200

    (categoryDash) created view + form to edit categor

---
+++ view/security/categoryDashboard.php
@@ -0,0 +1,25 @@
+<?php
+
+$category = $result['data']['category'];
+
+if (App\Session::isAdmin()) {
+
+?>
+
+<h1>Edit category (<?= $category->getName() ?>)</h1>
+
+<form action="index.php?ctrl=security&action=editCategory&id=<?=$category->getId()?>" method="POST" autocomplete="off">
+
+    <input type="text" name="category" value="<?=$category->getName()?>"required>
+
+    <input type="submit" name="submit" value="Edit">
+
+</form>
+
+<a href="index.php?ctrl=security&action=categoriesDashboard">Return</a>
+
+<?php } else { ?>
+
+    <h1>You're not allowed to be here !</h1>
+
+<?php } ?>
