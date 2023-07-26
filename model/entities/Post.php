99aab21fb03d522473b26e721ad053e2971161f2
Charlydcn <charly.ducournau@outlook.fr>
2023-07-26 14:55:09 +0200

    (entities/post) added $modificationDate attribute

--- model/entities/Post.php
+++ model/entities/Post.php
@@ -11,95 +11,115 @@ final class Post extends Entity
         private $title;
         private $content;
         private $creationDate;
+        private $modificationDate;
         private $topic;
         private $user;
 
         public function __construct($data)
         {
-                $this->hydrate($data);
+            $this->hydrate($data);
         }
 
         // ID *********************************************************************
 
         public function getId()
         {
-                return $this->id;
+            return $this->id;
         }
 
         public function setId($id)
         {
-                $this->id = $id;
+            $this->id = $id;
 
-                return $this;
+            return $this;
         }
 
         // CONTENT *********************************************************************
 
         public function getContent()
         {
-                return $this->content;
+            return $this->content;
         }
 
         public function setContent($content)
         {
-                $this->content = $content;
+            $this->content = $content;
 
-                return $this;
+            return $this;
         }
 
         // TITLE *********************************************************************
 
         public function getTitle()
         {
-                return $this->title;
+            return $this->title;
         }
 
         public function setTitle($title)
         {
-                $this->title = $title;
+            $this->title = $title;
 
-                return $this;
+            return $this;
         }
 
         // DATE *********************************************************************
 
         public function getCreationDate()
         {
-                $formattedDate = $this->creationDate->format("d/m/Y, H:i:s");
-                return $formattedDate;
+            $formattedDate = $this->creationDate->format("d/m/Y, H:i:s");
+            return $formattedDate;
         }
 
         public function setCreationDate($creationDate)
         {
-                $this->creationDate = new \DateTime($creationDate);
-                return $this;
+            $this->creationDate = new \DateTime($creationDate);
+            return $this;
+    }
+
+        // MODIFICATION DATE *********************************************************************
+
+        public function getModificationDate()
+        {
+            if ($this->modificationDate instanceof \DateTime) {
+                    return $this->modificationDate->format("d/m/Y, H:i:s");
+            } else {
+                    return null;
+            }
+    }
+
+
+        public function setModificationDate($modificationDate)
+        {
+            $this->modificationDate = $modificationDate ? new \DateTime($modificationDate) : null;
+            return $this;
         }
+
 
         // TOPIC *********************************************************************
 
         public function getTopic()
         {
-                return $this->topic;
+            return $this->topic;
         }
 
         public function setTopic($topic)
         {
-                $this->topic = $topic;
+            $this->topic = $topic;
 
-                return $this;
+            return $this;
         }
 
         // USER *********************************************************************
 
         public function getUser()
         {
-                return $this->user;
+            return $this->user;
         }
 
         public function setUser($user)
         {
-                $this->user = $user;
+            $this->user = $user;
 
-                return $this;
+            return $this;
         }
 }
