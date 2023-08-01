f833eef433423043744a849b6c78f90fbee15673
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-01 17:01:29 +0200

    (postsviews)remved script to put in separated file

--- view/forum/postsByTopic.php
+++ view/forum/postsByTopic.php
@@ -60,30 +60,4 @@ if(App\Session::getUser() && App\Session::getUser()->getBan() == 0) {
 
 <?php } ?>
 
-<script>
-    // DOMContentLoaded = le script sera exécuté lorsque l'ensemble du document HTML sera chargé et prêt
-document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionnez tous les icônes 'fa-pencil' avec la classe '.edit-icon'
-    const editIcons = document.querySelectorAll('.fa-pencil');
-
-    editIcons.forEach(function(editIcon) {
-        // Pour chaque icône, ajoutez un gestionnaire d'événement au clic
-        editIcon.addEventListener('click', function() {
-            // postId = l'id stocké dans le bouton edit <i> : data-post-id="$post->getId()" sur lequel on clique
-            // 'this.' permet de cibler l'élément sur lequel on clique donc on récupère le data-post-id stocké dans le <i>
-            // sur lequel on clique pour ensuite l'utiliser sur const editForm qui permet de récupérer chaque form correspondant
-            // pour leur ajouter/retirer la classe hidden/visible
-            const postId = this.getAttribute('data-post-id');
-            const editForm = document.getElementById('editForm-' + postId);
-
-            if (editForm.classList.contains('visible')) {
-                editForm.classList.add('hidden');
-                editForm.classList.remove('visible');
-            } else {
-                editForm.classList.add('visible');
-                editForm.classList.remove('hidden');
-            }
-        });
-    });
-});
-</script>
+<script src="<?= PUBLIC_DIR ?>/js/post.js"></script>
