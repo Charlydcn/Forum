2b5c2d0ff8092e8596df38e50396503bac302d72
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-01 17:01:40 +0200

    (userDetails) added css link

--- view/user/userDetails.php
+++ view/user/userDetails.php
@@ -2,11 +2,15 @@
 
 $user = $result['data']['user'];
 
+$css = 'userDetails.css';
+
 // on vérifie qu'il y a un utilisateur en session
 if(App\Session::getUser($id)) {
     // puis on vérifie qu'il soit bien admin ou propriétaire du compte qu'il consulte
     if (App\Session::isAdmin() || $user->getId() === $_SESSION['user']->getId()) {
 ?>
+
+        <h2>User details</h2>
 
         <form action="index.php?ctrl=security&action=editUser&id=<?= $user->getId() ?>" method="POST" autocomplete="off">
             <label>
@@ -28,38 +32,40 @@ if(App\Session::getUser($id)) {
                 <input type="password" name="pass2" value="">
             </label>
 
-            <?php
-            if(App\Session::isAdmin()) {
-            ?>
+                <?php
+                if(App\Session::isAdmin()) {
+                ?>
+    
+                    <select name="role">
+                        <?php
+                        // si l'utilisateur connecté est un admin, alors la deuxième <option> sera user, et inversement
+                        $v = $user->getRole() == 'user' ? 'admin' : 'user';
+                        ?>
+                        <option value="<?= $user->getRole() ?>"><?= $user->getRole() ?></option>
+                        <option value="<?= $v ?>"><?= $v ?></option>
+                    </select>
+    
+                    <label>
+                        Banned : 
+                        <input type="checkbox" name="ban" <?= $user->getBan() == 1 ? 'checked' : '' ?>>
+                    </label>
+                <?php } else { ?>
+    
+                    <p>Banned : <?= $user->getBan() === '1' ? 'Yes' : 'No' ?></p>
+    
+                <?php } ?>
 
-                <select name="role">
-                    <?php
-                    // si l'utilisateur connecté est un admin, alors la deuxième <option> sera user, et inversement
-                    $v = $user->getRole() == 'user' ? 'admin' : 'user';
-                    ?>
-                    <option value="<?= $user->getRole() ?>"><?= $user->getRole() ?></option>
-                    <option value="<?= $v ?>"><?= $v ?></option>
-                </select>
-
-                <label>
-                    Banned : 
-                    <input type="checkbox" name="ban" <?= $user->getBan() == 1 ? 'checked' : '' ?>>
-                </label>
-            <?php } else { ?>
-
-                <p>Banned : <?= $user->getBan() === '1' ? 'Yes' : 'No' ?></p>
-
-            <?php } ?>
-
-            <p>Member since : <?= $user->getRegistrationDate() ?></p>
-
+            
             <input type="submit" name="submit" value="Save changes">
-
+            
             <a href="index.php?ctrl=security&action=deleteUser&id=<?= $user->getId() ?>" class="delete-btn">
                 <i class="fa-solid fa-trash-can"></i>
             </a>
         </form>
-        <a href="index.php?ctrl=forum&action=listPostsByUser&id=<?=$user->getId()?>">Posts</a>
+
+        <p>Member since : <?= $user->getRegistrationDate() ?></p>
+
+        <a href="index.php?ctrl=forum&action=listPostsByUser&id=<?=$user->getId()?>">User's Posts</a>
 
 <?php }} else { ?>
 <!-- si l'utilisateur connecté n'est ni propriétaire du compte consulté ni admin, on affiche simplement les infos du compte -->
