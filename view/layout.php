45f02b642f81bd7bc8b776e0be359fecd832e86a
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-01 17:00:46 +0200

    (layout) new nav bar responsive (burger)

--- view/layout.php
+++ view/layout.php
@@ -8,8 +8,9 @@
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
     <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
-    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/<?=isset($css) ? $css : ""?>">
+    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/<?= $css === null ? "" : $css?>">
     <title>FORUM</title>
 </head>
 
@@ -17,47 +18,32 @@
     <div id="wrapper">
 
         <div id="mainpage">
-            <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
-            <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
-            <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
+            
             <header>
                 <nav>
                     <!-- RESPONSIVE NAV -->
-                    <div id="responsive-nav">
-                        <div id="sidenav">
-                            <a href="#" id="closeBtn"><i class="fa-solid fa-xmark"></i></a>
-                            <ul>
-                                <li><a href="index.php?ctrl=home">Home</a></li>
-                                <?php
-                                if (App\Session::isAdmin()) {
-                                ?>
-                                    <li><a href="index.php?ctrl=forum&action=listUsers">Users</a></li>
-    
-                                <?php } ?>
-                                <li><a href="index.php?ctrl=forum&action=listCategories">Categories</a></li>
-                                <li><a href="index.php?ctrl=forum&action=listTopics">Topics</a></li>
-                            </ul>
-                        </div>
-                        <i class="fa-solid fa-bars" id="openBtn"></i>
+                    <div id="openBtn">
+                        <span></span>
+                    </div>
 
-                        <a href="index.php?ctrl=home">FORUM</a>
+                    <a href="index.php?ctrl=home">FORUM</a>
 
-                        <div>
+                    <!-- user login/register/details -->
+                    <div id="user-nav">
+                    <?php
+                        if (App\Session::getUser()) {
+                        ?>
+
+                            <a href="index.php?ctrl=forum&action=userDetails&id=<?= App\Session::getUser()->getId() ?>"><i class="fas fa-user"></i></a>
+                            <a href="index.php?ctrl=security&action=logOff&id=<?= App\Session::getUser()->getId() ?>">Log off</a>
+
                         <?php
-                            if (App\Session::getUser()) {
-                            ?>
+                        } else {
+                        ?>
+                            <a href="index.php?ctrl=security&action=displayLogin">Log in</a>
+                            <a href="index.php?ctrl=security&action=displayRegister">Register</a>
 
-                                <a href="index.php?ctrl=forum&action=userDetails&id=<?= App\Session::getUser()->getId() ?>"><i class="fas fa-user"></i></a>
-                                <a href="index.php?ctrl=security&action=logOff&id=<?= App\Session::getUser()->getId() ?>">Log off</a>
-
-                            <?php
-                            } else {
-                            ?>
-                                <a href="index.php?ctrl=security&action=displayLogin">Log in</a>
-                                <a href="index.php?ctrl=security&action=displayRegister">Register</a>
-
-                            <?php } ?>
-                        </div>
+                        <?php } ?>
                     </div>
 
                     <!-- DESKTOP NAV -->
@@ -94,18 +80,36 @@
 
                     </div>
                     
-                    
                 </nav>
+                <div id="menu">
+                    <ul>
+                        <li><a href="index.php?ctrl=home">Home</a></li>
+                        <?php
+                        if (App\Session::isAdmin()) {
+                        ?>
+                            <li><a href="index.php?ctrl=forum&action=listUsers">Users</a></li>
+
+                        <?php } ?>
+                        <li><a href="index.php?ctrl=forum&action=listCategories">Categories</a></li>
+                        <li><a href="index.php?ctrl=forum&action=listTopics">Topics</a></li>
+                    </ul>
+                </div>
             </header>
+
+            <!-- c'est ici que les messages (erreur ou succès) s'affichent-->
+            <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
+            <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
 
             <main id="forum">
                 <?= $page ?>
             </main>
         </div>
+        
         <footer>
             <p>&copy; 2020 - Forum CDA - <a href="/home/forumRules.html">Forum's regulations</a> - <a href="">Legal notices</a></p>
         </footer>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous">
     </script>
     <script src="<?= PUBLIC_DIR ?>/js/script.js"></script>
@@ -135,6 +139,12 @@
                 content_css: '//www.tiny.cloud/css/codepen.min.css'
             });
         })
+
+        $(document).ready(function(){
+            $('#nav-icon1,#nav-icon2,#openBtn,#nav-icon4').click(function(){
+                $(this).toggleClass('open');
+            });
+        });
     </script>
 </body>
 
