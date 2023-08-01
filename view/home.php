443e4c59640abb5d59ce5b1ea47ebfe0d16003aa
Charlydcn <Charly.ducournau@outlook.fr>
2023-08-01 11:00:15 +0200

    (v/home) added 10 latest posts to homepage

--- view/home.php
+++ view/home.php
@@ -1,1 +1,37 @@
-<h1>Welcome to the Forum</h1>
+<?php
+$posts = $result["data"]['posts'];
+$css = 'posts.css';
+?>
+
+<h2>Welcome to the forum</h2>
+
+<h3>Latest posts :</h3>
+
+<?php
+foreach ($posts as $post) {
+    $topic = $post->getTopic();
+?>
+
+<div class="forumPost">
+            <div class="post-header"> <!-- POST HEADER -->
+                <a href="index.php?ctrl=forum&action=userDetails&id=<?=$post->getUser()->getId()?>">
+                    <?= "(Topic : " . $post->getTopic()->getTitle() . ") " . $post->getUser()->getUsername()?>
+                </a>
+        
+                <?= $post->getCreationDate() ?>
+                
+                <?php
+                if(App\Session::getUser() && App\Session::getUser()->getBan() == 0) {
+                    if($post->getUser()->getId() === $_SESSION['user']->getId() || App\Session::isAdmin()) {        
+                ?>
+                
+                <?php }} ?>
+            </div>
+        
+            <div class="post-content"> <!-- POST MAIN -->
+                <p><?= html_entity_decode($post->getContent()) ?></p>
+                <?= $post->getModificationDate() !== null ? "<p>(Modified : " . $post->getModificationDate() . ")</p>" : ''; ?>
+            </div>  
+        </div>
+
+<?php } ?>
