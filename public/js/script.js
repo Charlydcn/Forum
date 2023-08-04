432b305715b6ffdbc2b3203d774b02eab1837e98
charlydcn <Charly.ducournau@outlook.fr>
2023-08-04 09:38:15 +0200

    script.js: changed nav-menu name

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 var openBtn = document.getElementById("openBtn");
-var overlay = document.getElementById("menu");
+var overlay = document.getElementById("nav-menu");
 openBtn.addEventListener("click", function () {
 	this.classList.toggle("close");
 	overlay.classList.toggle("overlay");
