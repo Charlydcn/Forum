84043b114586f29392065a1955bed8fba7bfca13
charlydcn <Charly.ducournau@outlook.fr>
2023-08-04 11:07:37 +0200

    v/home: fixed href action on topic (listPosts..)

--- view/home.php
+++ view/home.php
@@ -19,7 +19,7 @@ if($posts !== null) {
                 <div class="post-header">
 
                     <!-- topic -->
-                    <a href="index.php?ctrl=forum&action=postsByTopic&id=<?=$topic->getId()?>">
+                    <a href="index.php?ctrl=forum&action=listPostsByTopic&id=<?=$topic->getId()?>">
                         <?= "<span>" . $topic->getTitle() . "</span>" ?>
                     </a>
                     <br>
