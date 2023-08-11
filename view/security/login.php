6d5fbfa8a284b39ac22edb952040aa8ea9afbbaf
charlydcn <Charly.ducournau@outlook.fr>
2023-08-11 12:23:30 +0200

    (auth views) added div for sementic purposes

--- view/security/login.php
+++ view/security/login.php
@@ -5,13 +5,15 @@ $css = 'auth.css';
 <form action="index.php?ctrl=security&action=login" method="POST" autocomplete="off">
     <h2>Log in</h2>
     
-    <label>E-Mail : 
-    <input type="email" name="email" required>
-    </label>
-
-    <label>Password : 
-    <input type="password" name="password" required>
-    </label>
+    <div>
+        <label>E-Mail : 
+            <input type="email" name="email" required>
+        </label>
+    
+        <label>Password : 
+            <input type="password" name="password" required>
+        </label>
+    </div>
 
     <input type="submit" name="submit" value="Log in">
     
--- view/security/register.php
+++ view/security/register.php
@@ -4,18 +4,22 @@ $css = 'auth.css';
 
 <form action="index.php?ctrl=security&action=register" method="POST" autocomplete="off">
     <h2>Join us</h2>
-    <label>Username
-        <input type="text" name="username" required>
-    </label>
-    <label>E-Mail
-        <input type="email" name="email" required>
-    </label>
-    <label>Password <span>(8-25 characters)</span>
-        <input type="password" name="pass1" required>
-    </label>
-    <label>Confirm password
-        <input type="password" name="pass2" required>
-    </label>
+
+    <div>
+        <label>Username : 
+            <input type="text" name="username" required>
+        </label>
+        <label>E-Mail : 
+            <input type="email" name="email" required>
+        </label>
+        <label>Password  : 
+            <input type="password" name="pass1" required>
+        </label>
+        <label>Confirm password : 
+            <input type="password" name="pass2" required>
+        </label>
+    </div>
+    
     <input type="submit" name="submit" value="Register">
 
     <p>Already registered ? <a href="index.php?ctrl=security&action=displayLogin">Log in</a></p>
